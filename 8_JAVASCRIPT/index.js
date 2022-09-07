d0a8a37d96585354c9379d118f881040c0ad6fdc
maulana misber <maulana.misber@gmail.com>
2022-09-07 19:44:41 +0700

    Javascript

---
+++ 8_JAVASCRIPT/index.js
@@ -0,0 +1,30 @@
+var a = 5;
+let b = "kampus merdeka";
+const nama = "Budi";
+let terdaftar = false;
+let lengkap_arr =[a, b, nama, terdaftar];
+
+Function perkenalan (){
+    let asal = "indonesia";
+    return console.log(
+    "perkenalan nama saya" +
+    nama +
+    " nomor urut"
+    a +
+    "sekarang sedang mengukuti" +
+    b +
+    "berasal dari"
+    asal
+
+    );
+}
+
+i (terdaftar === true){
+console.log(nama + "terdaftar sebagai kegiatan kampus merdeka")
+}
+
+a = b;
+nama = b;
+
+console.log("array = " + nama);
+console.log("a adalah = ")+a);
